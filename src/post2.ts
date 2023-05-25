2a67d130fd3512bdb1de7078e5df7f0325b5057e
liw003 <liw003>
2023-05-25 16:45:33 +0800

    sign

--- src/post2.ts
+++ src/post2.ts
@@ -1,6 +1,82 @@
 /// 通过token上传
 
 (function a() {
+  async function genSign(content :string){
+    var MINZER0 = 15
+    var t1 = Date.now()
+    var time = ('' + t1).substring(0,10)
+
+    var pre = time + content;
+    var te = new TextEncoder
+
+    var rnd = new Uint8Array(32)
+    while (true) {
+      await crypto.getRandomValues(rnd)
+      var uuid = btoa(rnd as unknown as  string).substring(0,32)
+      let d = await crypto.subtle.digest("SHA-256",te.encode(pre + uuid))  
+      d = await crypto.subtle.digest("SHA-256",d)  
+      let ui8 = new Uint8Array(d) ;
+      // 计算开头bit0数量
+      var Cnt = 0
+      for (let index = 0; index < 32; index++) {
+        const n = ui8[index];
+        if ((n & 128) == 0){
+          Cnt += 1
+        }else{
+          break
+        }
+        if ((n & 64) == 0){
+          Cnt += 1
+        }else{
+          break
+        }
+        if ((n & 32) == 0){
+          Cnt += 1
+        }else{
+          break
+        }
+        if ((n & 16) == 0){
+          Cnt += 1
+        }else{
+          break
+        }
+        if ((n & 8) == 0){
+          Cnt += 1
+        }else{
+          break
+        }
+        if ((n & 4) == 0){
+          Cnt += 1
+        }else{
+          break
+        }
+
+        if ((n & 2) == 0){
+          Cnt += 1
+        }else{
+          break
+        }
+
+        if ((n & 1) == 0){
+          Cnt += 1
+        }else{
+          break
+        }
+
+        if(Cnt>= MINZER0){
+          break
+        }
+      }
+
+      if(Cnt >= MINZER0){
+        console.log(MINZER0,time + uuid, Date.now() -t1,)
+
+        return {time,sign:uuid}
+        
+      } 
+    }
+  }
+
   var posttoken = "";
   var initcontent = "";
   (function(){
@@ -47,7 +123,7 @@
     return (document.getElementById(id) as HTMLInputElement).value;
   }
 
-  function submit() {
+  async function submit() {
     var content = getValue("content-input");
     if (!content || content == initcontent) {
       showMsg("please input content");
@@ -55,7 +131,8 @@
     }
 
     showLoading(true);
-    var bodyObj = { content: content ,token:posttoken} as any;
+    let s = await genSign(content)
+    var bodyObj = { content: content ,token:posttoken,...s} as any;
  
 
     var body = JSON.stringify(bodyObj);
